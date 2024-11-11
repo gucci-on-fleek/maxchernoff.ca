c4c279b6c6838412403a7839368e7c201184d7f1
Max Chernoff <git@maxchernoff.ca>
2024-11-11 04:54:16 -0700

    Automatically update server

--- web/flask/app.py
+++ web/flask/app.py
@@ -8,30 +8,48 @@
 ### Imports ###
 ###############
 
-from traceback import format_exception
 from datetime import datetime
+from hashlib import sha256
+from hmac import compare_digest
+from hmac import new as hmac_new
+from os import environ as env
+from pathlib import Path
+from traceback import format_exception
 
 from flask import Flask, Response, request
 from waitress import serve
+from werkzeug.exceptions import HTTPException as _HTTPException
 
 #################
 ### Constants ###
 #################
 
-ZONE = datetime.now().astimezone().tzinfo
-
-
-######################
-### Initialization ###
-######################
-
 app = Flask(__name__)
+
+ZONE = datetime.now().astimezone().tzinfo
+UPDATE_TRIGGER = Path("/root/triggers/install-repo.trigger")
+
+
+#########################
+### Utility Functions ###
+#########################
+
+
+class HTTPException(_HTTPException):
+    """Custom HTTPException class for handling errors."""
+
+    def __init__(self, code: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.code = code
 
 
 @app.errorhandler(Exception)
-def error_handler(error):
+def error_handler(error: HTTPException) -> Response:
     """Return a minimal response for all errors."""
-    if 500 <= error.code < 600:
+    if not getattr(error, "code", None):
+        error.code = 500
+
+    if 500 <= (error.code or 500) < 600:
         print(f"==== {request.url} ({error.code} {error.name}) ====")
         print("".join(format_exception(error)))
 
@@ -40,6 +58,35 @@ def error_handler(error):
         status=error.code,
         mimetype="text/plain",
     )
+
+
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        raise HTTPException(403)
+
+    hash_object = hmac_new(
+        secret_token.encode("utf-8"),
+        msg=payload_body,
+        digestmod=sha256,
+    )
+    expected_signature = "sha256=" + hash_object.hexdigest()
+
+    if not compare_digest(expected_signature, signature_header):
+        raise HTTPException(403)
+
+
+##########################
+### Endpoint Functions ###
+##########################
 
 
 @app.route("/status")
@@ -54,11 +101,22 @@ def status():
     }
 
 
-##########################
-### Endpoint Functions ###
-##########################
+@app.route("/update-server", methods=["POST"])
+def update_server():
+    """Updates the server with the latest changes."""
 
-# Nothing here yet
+    # Verify the request was sent from GitHub
+    verify_signature(
+        payload_body=request.get_data(),
+        secret_token=env["WEBHOOK_SECRET"],
+        signature_header=request.headers.get("x-hub-signature-256"),
+    )
+
+    # Update the server
+    with open(UPDATE_TRIGGER, "wt") as trigger:
+        trigger.write(datetime.now().isoformat())
+
+    return {"status": "ok"}
 
 
 ###################
