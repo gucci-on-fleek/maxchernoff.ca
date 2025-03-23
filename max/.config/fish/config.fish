1e6747b83448f4022392cd0cb83d776ef7256f5c
Max Chernoff <git@maxchernoff.ca>
2025-03-23 03:37:46 -0600

    Remove nspawn function

--- max/.config/fish/config.fish
+++ max/.config/fish/config.fish
@@ -19,10 +19,6 @@ if set --query TOOLBOX_PATH
     set --global fish_color_user --reverse --bold --dim yellow
 end
 
-if test "$in_nspawn" = "true"
-    set --global fish_color_user --reverse --bold --dim magenta
-end
-
 function ok
     truncate --size=0 ~repo/triggers/get-status.output
     echo "$(date)" > ~repo/triggers/get-status.trigger
@@ -41,20 +37,6 @@ function container
     # podman run --name=fedora -it --network=host --volume=/var/home/max:/home/max --security-opt=label=disable fedora:latest
     podman start fedora
     podman exec -lit /usr/bin/fish
-end
-
-# systemd-nspawn
-function nspawn
-    sudo systemd-nspawn \
-        --background='' \
-        --console='autopipe' \
-        --directory='/' \
-        --ephemeral \
-        --machine='EPHEMERAL' \
-        --resolv-conf='bind-stub' \
-        --setenv='in_nspawn=true' \
-        --user='max' \
-        /usr/bin/fish --login $argv
 end
 
 function refresh
