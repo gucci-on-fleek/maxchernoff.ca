0daae6dec92c48435c10045d71895a211a6db1b3
Max Chernoff <git@maxchernoff.ca>
2025-05-18 02:17:38 -0600

    Add Anubis to ViewVC image

--- builder/containers/viewvc/start.py
+++ builder/containers/viewvc/start.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: MPL-2.0+ OR CC-BY-SA-4.0+
 # SPDX-FileCopyrightText: 2024 Max Chernoff
 
-from os import nice
+from os import environ, nice
 from subprocess import Popen
 
 import sapi
@@ -19,6 +19,29 @@ def application(environ, start_response):
     viewvc.main(server, cfg)
     return []
 
+
+# Start Anubis
+Popen(
+    [
+        "/usr/local/bin/anubis",
+    ],
+    env={
+        **environ,
+        "BIND": "127.0.0.1:8923",  # Default value
+        "COOKIE_DOMAIN": "tug.org",
+        "COOKIE_EXPIRATION_TIME": "720h",  # 30 days, increased from default of 1 week
+        "DIFFICULTY": "5",  # Increase from default of 4
+        "WEBMASTER_EMAIL": "webmaster@tug.org",
+        "TARGET": "http://localhost:43219",
+        "REDIRECT_DOMAINS": "svn.tug.org:8369",
+
+        # We'll hardcode the private key here because we're only being targeted
+        # by naive bots; anyone who cared enough to find this would be smart
+        # enough to simply use the SVN protocol instead of scraping the web
+        # interface.
+        "ED25519_PRIVATE_KEY_HEX": "3e5fabb2b118e31bbafc2356b7cd39874b1f7ccc1622e45fdd196ed55b9f102b",
+    }
+)
 
 # Start the Caddy server
 Popen(
@@ -44,9 +67,8 @@ serve(
     backlog=8,
     outbuf_overflow=20 * 1024**2,
     inbuf_overflow=20 * 1024**2,
-    connection_limit=20,
-    cleanup_interval=10,
-    channel_timeout=10,
-    max_request_body_size=10 * 1024,
-    channel_request_lookahead=2,
+    expose_tracebacks=True,
+    connection_limit=24,
+    cleanup_interval=30,
+    channel_timeout=30,
 )
