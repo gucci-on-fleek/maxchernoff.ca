859914246cf2240bc7f965dcff060f31f136cafb
Max Chernoff <git@maxchernoff.ca>
2025-11-13 04:54:37 -0700

    Enable fs-verity for the OS container image

--- builder/containers/fedora-iot/container-build.sh
+++ builder/containers/fedora-iot/container-build.sh
@@ -8,11 +8,45 @@ set -euxo pipefail
 # Keep a cache to speed up the build
 cache=/var/cache/rpm-ostree
 
-# Build and push the container
+# Build the container
 rpm-ostree compose image \
     --initialize-mode=query \
-    --format=registry \
+    --format=ociarchive \
     --cachedir=$cache \
     --max-layers=200 \
     /root/source/fedora-iot.yaml \
-    localhost:!!registry.port!!/fedora-iot:latest
+    fedora-iot.ociarchive
+
+# Get the composefs info
+composefs_repo=/var/tmp/composefs-repo
+mkdir -p "$composefs_repo"
+
+image_id="$(\
+    bootc internals cfs \
+        --repo="$composefs_repo" \
+        oci pull oci-archive:fedora-iot.ociarchive \
+    | grep --only-matching --perl-regexp '(?<=^Fetching config ).*$'\
+)"
+
+fsverity_id="$(\
+    bootc internals cfs \
+        --repo="$composefs_repo" \
+        oci compute-id "$image_id" \
+)"
+
+# Rebuild the container with composefs fsverity info
+podman build \
+    --no-cache \
+    --security-opt=label=disable \
+    --volume=/var/cache/libdnf5/:/var/cache/libdnf5/:rw \
+    --build-arg="COMPOSEFS_FSVERITY=$fsverity_id" \
+    --label=containers.composefs.fsverity="$fsverity_id" \
+    --file=/root/source/Containerfile \
+    --iidfile=./image-id \
+    --tag=maxchernoff.ca/fedora-iot:latest \
+    "."
+
+# Push the container
+skopeo copy \
+    containers-storage:maxchernoff.ca/fedora-iot:latest \
+    oci-archive:/root/output/fedora-iot.ociarchive
