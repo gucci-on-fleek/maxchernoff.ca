6f5a1b237bc2a06c85d5ec40a7c4dc58944e605c
Max Chernoff <git@maxchernoff.ca>
2026-07-11 16:02:04 -0600

    Prune /root/ from containers

--- builder/containers/tex/tex-cache.sh
+++ builder/containers/tex/tex-cache.sh
@@ -12,36 +12,36 @@ ctx=/opt/context/texmf-linux-64/bin
 {
     cd "$(mktemp --directory)"
     $tl/luaotfload-tool --update || exit 1
-    $tl/lualatex /root/make-font-cache/latex-cache.tex || exit 1
-    $tl/lualatex /root/make-font-cache/latex-cache.tex || exit 1
+    $tl/lualatex /opt/make-font-cache/latex-cache.tex || exit 1
+    $tl/lualatex /opt/make-font-cache/latex-cache.tex || exit 1
 } &
 {
     cd "$(mktemp --directory)"
     $tl/context --make > /dev/null || exit 1
     $tl/mtxrun --script fonts --reload > /dev/null || exit 1
-    $tl/context /root/make-font-cache/context-cache.tex || exit 1
+    $tl/context /opt/make-font-cache/context-cache.tex || exit 1
 } &
 {
     cd "$(mktemp --directory)"
     $tl/context --luatex --make > /dev/null || exit 1
     $tl/mtxrun --luatex --script fonts --reload > /dev/null || exit 1
-    $tl/context --luatex /root/make-font-cache/context-cache.tex || exit 1
+    $tl/context --luatex /opt/make-font-cache/context-cache.tex || exit 1
 } &
 {
     cd "$(mktemp --directory)"
     $ctx/context --make > /dev/null || exit 1
     $ctx/mtxrun --script fonts --reload > /dev/null || exit 1
-    $ctx/context /root/make-font-cache/context-cache.tex || exit 1
+    $ctx/context /opt/make-font-cache/context-cache.tex || exit 1
 } &
 {
     cd "$(mktemp --directory)"
     $ctx/context --luatex --make > /dev/null || exit 1
     $ctx/mtxrun --luatex --script fonts --reload > /dev/null || exit 1
-    $ctx/context --luatex /root/make-font-cache/context-cache.tex || exit 1
+    $ctx/context --luatex /opt/make-font-cache/context-cache.tex || exit 1
 } &
 
 wait
 
 # Remove the temporary files
 shopt -s extglob
-rm -rf /root/make-font-cache/!(*.tex)
+rm -rf /opt/make-font-cache/!(*.tex)
