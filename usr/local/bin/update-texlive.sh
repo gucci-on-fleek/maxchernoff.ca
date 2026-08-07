a9c592f7c5d4a994416f0a8992dd741c3e14c90d
Max Chernoff <git@maxchernoff.ca>
2026-08-07 04:03:27 -0600

    TL fixes

--- usr/local/bin/update-texlive.sh
+++ usr/local/bin/update-texlive.sh
@@ -8,12 +8,11 @@ set -euo pipefail
 # Set the environment variables
 export PATH="/var/home/tex/texlive/bin/x86_64-linux:/usr/local/bin:/usr/bin"
 
-# Update the TeX Live installation. Anything that depends on XeTeX will fail
-# because we've uninstalled it, so ignore any errors.
-tlmgr update --all --self || true
+# Update the TeX Live installation.
+tlmgr update --all --self
 
 # Reset the ls-R dates so that ConTeXt doesn't auto-remake its formats
-touch --date='2 days ago' /var/home/tex/texlive/texmf-{config,dist,local,var}/ls-R
+touch --date='@0' /var/home/tex/texlive/texmf-{config,dist,local,var}/ls-R
 
 # Remake the ConTeXt formats
 context --make > /dev/null
